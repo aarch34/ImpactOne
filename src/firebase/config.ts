c0f4e671bae099f66138ec9251d52e071e320be3
gigacat-25 <pabt2005@gmail.com>
2026-02-03 01:39:31 +0530

    chore: Remove Firebase dependency and enable TypeScript and ESLint build error enforcement in Next.js configuration.

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-1580483224-bf99e",
-  "appId": "1:1088664587549:web:24872bd19c1a0b9df265f4",
-  "apiKey": "AIzaSyDpG-IYwE9RTrGrfo0Bkh88wVuIik0aO0s",
-  "authDomain": "studio-1580483224-bf99e.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1088664587549"
-};
